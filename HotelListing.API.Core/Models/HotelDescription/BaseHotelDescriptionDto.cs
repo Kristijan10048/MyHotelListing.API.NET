10e4e96b3b5a2c6bf87380557d3953901b3f184b
Kristijan <kristijan.petkov@gmail.com>
2023-11-27 20:54:03 +0100

    Added fist test and description DTO

---
+++ HotelListing.API.Core/Models/HotelDescription/BaseHotelDescriptionDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.API.Core.Models.HotelDescription
+{
+    /// <summary>
+    /// Base class hotel 
+    /// </summary>
+    public abstract class BaseHotelDescriptionDto : IHotelDescriptionDto
+    {
+        #region Public Properties
+
+        public int Id { get; set; }
+
+        #region Optiional fileds
+
+        public bool? HasSpa { get; set; }
+        public bool? HasNonSmokingRooms { get; set; }
+        public bool? HasFitnessCenter { get; set; }
+        public bool? FreeWiFi { get; set; }
+
+        public bool? PrivateParking { get; set; }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ HotelListing.API.Core/Models/HotelDescription/CreateHotelDescriptionDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.API.Core.Models.HotelDescription
+{
+    public class CreateHotelDescriptionDto
+    {
+       
+        
+        #region Optiional fileds
+
+        public bool? HasSpa { get; set; }
+        public bool? HasNonSmokingRooms { get; set; }
+        public bool? HasFitnessCenter { get; set; }
+        public bool? FreeWiFi { get; set; }
+
+        public bool? PrivateParking { get; set; }
+        #endregion
+    }
+}
